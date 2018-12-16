0eb2bef6f0cc84a851e9d961037e3504d5982e39
Jobins John <jobins666@gmail.com>
2018-12-16 16:01:36 +0400

    Initial Commit

---
+++ BitOP/BitOPTests/BitOPTests.swift
@@ -0,0 +1,34 @@
+//
+//  BitOPTests.swift
+//  BitOPTests
+//
+//  Created by Jobins John on 12/16/18.
+//  Copyright © 2018 Jobins John. All rights reserved.
+//
+
+import XCTest
+@testable import BitOP
+
+class BitOPTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
