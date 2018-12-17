7bebd286e2dfbd108b032de305a6d94f779e1082
Jobins John <jobins666@gmail.com>
2018-12-17 10:52:20 +0400

    Folder and new VC's added

---
+++ BitOP/BitOP/Controller/Login/BitOPLoginVC.swift
@@ -0,0 +1,30 @@
+//
+//  BitOPLoginVC.swift
+//  BitOP
+//
+//  Created by Jobins John on 12/17/18.
+//  Copyright © 2018 Jobins John. All rights reserved.
+//
+
+import UIKit
+
+class BitOPLoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
