f18583456a6e17a06ff0ae7271f224b7d60872a0
Jobins John <jobins666@gmail.com>
2018-12-18 15:27:13 +0400

    Added Extension for textField & Ui Color

---
+++ BitOP/BitOP/Extensions/UiTextField+Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  UiTextField+Extensions.swift
+//  BitOP
+//
+//  Created by Jobins John on 12/18/18.
+//  Copyright © 2018 Jobins John. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+extension UITextField {
+    
+    @IBInspectable var paddingLeft: CGFloat {
+        get {
+            return leftView!.frame.size.width
+        }
+        set {
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: newValue, height: frame.size.height))
+            leftView = paddingView
+            leftViewMode = .always
+        }
+    }
+    
+    @IBInspectable var paddingRight: CGFloat {
+        get {
+            return rightView!.frame.size.width
+        }
+        set {
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: newValue, height: frame.size.height))
+            rightView = paddingView
+            rightViewMode = .always
+        }
+    }
+}
