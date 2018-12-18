dc533df9c92cd13abcd57a049d63736fdd5f2355
Jobins John <jobins666@gmail.com>
2018-12-18 15:27:38 +0400

    Extensions added

---
+++ BitOP/BitOP/View/Global Constants/BitOpConstants.swift
@@ -0,0 +1,34 @@
+//
+//  BitOpConstants.swift
+//  BitOP
+//
+//  Created by Jobins John on 12/18/18.
+//  Copyright © 2018 Jobins John. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+
+let APP_MAIN_BUTTON_COLOR   = "#0056A0"
+
+let APP_MAIN_TEXTFIELD_BORDER_COLOR   = "#BCE0FD"
+
+let APP_MAIN_TEXTFIELD_TEXT_COLOR   = "#333333"
+
+let APP_MAIN_TEXTFIELD_PLACEHOLDER_COLOR   = "#a2c0d6"
+
+let APP_MAIN_TEXTFIELD_PLACEHOLDER_LEFT_PADDING  = 7.0
+
+let APP_MAIN_TEXTFIELD_PLACEHOLDER_RIGHT_PADDING  = 7.0
+
+let APP_MAIN_TEXTFIELD_BORDER_WIDTH   = 1.0
+
+let APP_MAIN_TEXT_FONT_COLOR   = "#333333"
+
+let WHITE_COLOR     = "#FFFFFF"
+
+let BLACK_COLOR     = "#000000"
+
+let GREY_COLOR      = "#908F8F"
