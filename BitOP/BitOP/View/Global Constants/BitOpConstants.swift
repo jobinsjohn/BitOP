7ce8461c17a253d58eeee40b650e237ab4b1cc8e
Jobins John <jobins666@gmail.com>
2018-12-24 15:57:25 +0400

    Code Structured

--- BitOP/BitOP/View/Global Constants/BitOpConstants.swift
+++ BitOP/BitOP/View/Global Constants/BitOpConstants.swift
@@ -11,23 +11,13 @@ import UIKit
 
 
 let APP_MAIN_BUTTON_COLOR                           = "#0056A0"
-
 let APP_MAIN_TEXTFIELD_BORDER_COLOR                 = "#BCE0FD"
-
 let APP_MAIN_TEXTFIELD_TEXT_COLOR                   = "#333333"
-
 let APP_MAIN_TEXTFIELD_PLACEHOLDER_COLOR            = "#a2c0d6"
-
 let APP_MAIN_TEXTFIELD_PLACEHOLDER_LEFT_PADDING     = 7.0
-
 let APP_MAIN_TEXTFIELD_PLACEHOLDER_RIGHT_PADDING    = 7.0
-
 let APP_MAIN_TEXTFIELD_BORDER_WIDTH                 = 1.0
-
 let APP_MAIN_TEXT_FONT_COLOR                        = "#333333"
-
 let WHITE_COLOR                                     = "#FFFFFF"
-
 let BLACK_COLOR                                     = "#000000"
-
 let GREY_COLOR                                      = "#908F8F"
